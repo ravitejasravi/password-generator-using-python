91a2010edbfde12fa3f8b6ae9d76ea00722223c7
“ravitejats18” <ravitejats18@gmail.com>
2022-10-03 21:14:06 +0530

    initial commit

---
+++ password_generator.py
@@ -0,0 +1,46 @@
+import random, math
+
+
+def password_generator(length):
+    # we have taken alphabets as single character so it will be easy to convert it into list of alphabets
+    alphabets_string = "abcdefghijklmnopqrstuvwxyz" 
+
+    # converting that string into list of alphanets
+    alphabets_lower = [char for char in alphabets_string]
+
+
+    # list of alphabets in upper case
+    alphabets_upper = [char.upper() for char in alphabets_string] 
+
+
+    # numbers
+    numbers = "1234567890"
+    numbers_list = [num for num in numbers]
+
+    # special characters
+    special_chars = "!@#$%^&*()><?+_-[]/\:;,."
+    special_chars_list = [char for char in special_chars]
+
+    list_of_all_chars = alphabets_lower + special_chars_list + alphabets_upper + numbers_list
+    
+    # to mix all the characters,symbols,numbers
+    mixed_chars = []
+    while len(list_of_all_chars) != 0:
+        char = random.choice(list_of_all_chars)
+        if char not in mixed_chars:
+            mixed_chars.append(char)
+            list_of_all_chars.remove(char)
+
+    password = ""
+    for var in range(0,length):
+        password = password + random.choice(mixed_chars)
+
+    return password
+
+
+length = int(input("Enter the length(MIN:8) of your password: "))
+if length >= 8:
+    print(password_generator(length))
+else:
+    print("Minimum length of the password should be 8.")
+
